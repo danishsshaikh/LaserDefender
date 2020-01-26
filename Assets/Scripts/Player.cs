c2dfa1a88ef397b585e7e1737cd061cccdee5edf
danishsshaikh <danishsshaikh@gmail.com>
2020-01-26 20:19:19 +0530

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,5 +20,6 @@ public class Player : MonoBehaviour
     {
         var deltaX = Input.GetAxis("Horizontal");
         var newXPos = transform.position.x + deltaX;
+        transform.position = new Vector2(newXPos, transform.position.y);
     }
 }
