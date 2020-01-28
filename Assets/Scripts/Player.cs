75bd80bac08876f6fff882b5ab9ad9fddc8488cf
danishsshaikh <danishsshaikh@gmail.com>
2020-01-28 20:43:55 +0530

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        throw new NotImplementedException();
     }
 
     // Update is called once per frame
