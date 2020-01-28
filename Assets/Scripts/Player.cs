64f27af6258197c15175ff55a49f1c0f07474444
danishsshaikh <danishsshaikh@gmail.com>
2020-01-28 22:16:43 +0530

    y axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     float xMin;
     float xMax;
+    float yMin;
+    float yMax;
 
     void Start()
     {
@@ -19,6 +21,9 @@ public class Player : MonoBehaviour
         Camera gameCamera = Camera.main;
         xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
     }
 
     // Update is called once per frame
@@ -35,7 +40,7 @@ public class Player : MonoBehaviour
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
         var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
-        var newYPos = transform.position.y + deltaY;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         // X and Y
         transform.position = new Vector2(newXPos, newYPos);    
     }
