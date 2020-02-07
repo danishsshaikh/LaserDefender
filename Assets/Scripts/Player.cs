8c4ba0b9d6be0c9906086be32b306ced68aeb594
danishsshaikh <danishsshaikh@gmail.com>
2020-02-07 22:37:14 +0530

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,10 +32,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            //GameObject laser = 
-            //    Instantiate(laserPrefab, transform.position, Quaternion.identity) 
-            //    as GameObject;
-            //laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+            StartCoroutine(FireContinously());
 
         }
     }
