5f764b5fd1243b762277b7ece228c5d0c4870bab
danishsshaikh <danishsshaikh@gmail.com>
2020-03-18 16:40:15 +0530

    Enemy path fixed :-1:

--- Assets/EnemyPathing.cs
+++ Assets/EnemyPathing.cs
@@ -6,16 +6,67 @@ public class EnemyPathing : MonoBehaviour
 {
 
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
+        //Debug.Log(transform.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards
+                (transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //private void Move()
+    //{
+    //    if (waypointIndex <= waypoints.Count - 1)
+    //    {
+    //        var targetPosition = waypoints[waypointIndex].transform.position;
+    //        var movementThisFrame = moveSpeed * Time.deltaTime;
+
+    //        transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+    //        //Debug.Log("MOVED");
+    //        //Debug.Log(transform.position + targetPosition);
+
+
+    //        if (transform.position == targetPosition)
+    //        {
+    //            waypointIndex++;
+    //            //Debug.Log("WayPoint Index Increased" + " : "  + waypointIndex);
+    //            //Debug.Log(transform.position + targetPosition);
+    //        }
+    //        else
+    //        {
+    //            //if(waypointIndex == 3)
+    //            //{
+    //            //    Destroy(gameObject);
+    //            //    Debug.Log("Deleted" + gameObject);
+    //            //}
+    //            Destroy(gameObject);
+    //            //Debug.Log("Deleted" + " " + gameObject);
+    //        }
+    //    }
+    //}
 }
