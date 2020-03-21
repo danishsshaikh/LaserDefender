be6b73659d07aedda17762d4706ed6f2a846c9d6
danishsshaikh <danishsshaikh@gmail.com>
2020-03-21 17:23:01 +0530

    Create EnemySpawner.cs

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemryCount = 0; enemryCount < waveConfig.GetNumberOfEnemies(); enemryCount++)
+        {
+            Instantiate(
+            waveConfig.GetEnemyPrefab(),
+            waveConfig.GetWaypoints()[0].transform.position,
+            Quaternion.identity);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+        
+    }
+
+   
+}
