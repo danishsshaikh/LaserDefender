5053b590ce9068d58b2ea099a9cb3d5c453cb2f6
danishsshaikh <danishsshaikh@gmail.com>
2020-03-29 13:21:04 +0530

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] float showCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] GameObject projectile;
+
     void Start()
     {
-        
+        showCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        showCounter -= Time.deltaTime;
+        if (showCounter <= 0f)
+        {
+            Fire();
+            showCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser =
+                   Instantiate(projectile, transform.position, Quaternion.identity)
+                   as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
