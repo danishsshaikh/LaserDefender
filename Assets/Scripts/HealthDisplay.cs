5161311d42cd803e4c9b5a1213ec15e5ebf68852
danishsshaikh <danishsshaikh@gmail.com>
2020-03-31 17:51:01 +0530

    Create HealthDisplay.cs

---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    Text healthText;
+    Player player;
+    //GameSession gameSession;
+
+    private void Start()
+    {
+        healthText = GetComponent<Text>();
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        healthText.text = player.GetHealth().ToString();
+        //Debug.Log(healthText.text);
+    }
+}
