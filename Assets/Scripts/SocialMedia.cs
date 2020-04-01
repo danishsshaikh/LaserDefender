8487af91491dfc64e9fc5477960aa3d7bed4e71e
danishsshaikh <danishsshaikh@gmail.com>
2020-04-01 18:23:41 +0530

    :link: socialmedia

---
+++ Assets/Scripts/SocialMedia.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialMedia : MonoBehaviour
+{
+    public void Github()
+    {
+        Application.OpenURL("https://github.com/danishsshaikh");
+    }
+    public void Whatsapp()
+    {
+        Application.OpenURL("https://wa.me/91986770229");
+    }
+
+    public void LinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/danishsshaikh/");
+    }
+}
